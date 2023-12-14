6690eae31a1d0519387e45a292d548cb58a65a3c
aura syufee <114422139+lintangaura@users.noreply.github.com>
2023-12-14 19:43:54 +0700

    update progres baru

---
+++ app/src/main/java/com/bangkit23/estetika/ui/article/ArticleListActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit23.estetika.ui.article
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit23.estetika.R
+
+class ArticleListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_list)
+    }
+}
---
+++ app/src/main/java/com/bangkit23/estetika/ui/batik/BatikListActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit23.estetika.ui.batik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit23.estetika.R
+
+class BatikListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batik_list)
+    }
+}
